9ddaa1bde1f92729f495654bad5d3dbf8cacf319
bettafish15 <bettafish6@gmail.com>
2021-05-10 11:37:37 +0700

    :construction:

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 import inspect
-import tkinter as tk                # python 3
+import tkinter as tk  # python 3
 from tkinter import font
 from tkinter import ttk
 import algorithm
+
 
 class mainApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        self.title('Algorithm Visualizer')
-        self.geometry('320x100')
-        ttk.Label(self, text='Choose algorithm: ', font= font.Font(family='Times New Roman', size=10)).grid(column = 0, 
-          row = 5, padx = 10, pady = 25) 
+        self.title("Algorithm Visualizer")
+        self.geometry("320x100")
+        ttk.Label(
+            self,
+            text="Choose algorithm: ",
+            font=font.Font(family="Times New Roman", size=10),
+        ).grid(column=0, row=5, padx=10, pady=25)
 
         self.tkvar = tk.StringVar(self)
-        algoList = ttk.Combobox(self, state="readonly", width = 20, textvariable = self.tkvar)
-        
+        algoList = ttk.Combobox(
+            self, state="readonly", width=20, textvariable=self.tkvar
+        )
+
         array_temp = []
 
         for i, element in inspect.getmembers(algorithm):
-          if inspect.isclass(element):
-            array_temp.append(i)
+            if inspect.isclass(element):
+                array_temp.append(i)
 
-        algoList['values'] = tuple(array_temp)
+            algoList["values"] = tuple(array_temp)
         algoList.current(0)
-        algoList.grid(column = 1, row = 5)
+        algoList.grid(column=1, row=5)
 
-        tk.Button(self, text="Start", command=lambda: getattr(algorithm, self.tkvar.get())(self)).grid(column = 100, row = 5, padx = 5, pady = 5)
-        
+        tk.Button(
+            self,
+            text="Start",
+            command=lambda: getattr(algorithm, self.tkvar.get())(self),
+        ).grid(column=100, row=5, padx=5, pady=5)
+
+
 if __name__ == "__main__":
     app = mainApp()
     app.mainloop()
